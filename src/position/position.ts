55d6a951c83e71fc2b607e421f2bbe774e16676f
Joshua <j.joshua34@outlook.com>
2022-12-21 16:27:23 +0000

    adding a second piece with functionality, turns, and  tech debt

---
+++ src/position/position.ts
@@ -0,0 +1,221 @@
+import { getRowIndexArray, getColumnIndexArray } from "../helperFunctions/helperFunction";
+import { ColumnIds, RowIds, RowIndexs, BoardPosition } from "../types/boardTypes";
+import { KnightPositionBlueprint, PositionBlueprint } from "../types/positionTypes";
+
+class PositionParent implements BoardPosition {
+    columnId;
+    rowId
+    constructor(columnId: ColumnIds, rowId: RowIds) {
+        this.columnId = columnId;
+        this.rowId = rowId;
+    }
+}
+
+export class Position extends PositionParent implements PositionBlueprint {
+// eslint-disable-next-line @typescript-eslint/no-useless-constructor
+constructor(columnId: ColumnIds, rowId: RowIds) {
+    super(columnId, rowId)
+}
+moveUp() {
+    let rowIdNumber = parseInt(this.rowId); 
+    if(rowIdNumber >= getRowIndexArray().length) {
+        return null;
+    }
+    rowIdNumber++;
+    this.rowId = rowIdNumber.toString() as typeof RowIndexs [keyof typeof RowIndexs];
+    return this.rowId
+}
+moveDown() {
+    let rowIdNumber = parseInt(this.rowId);
+    if (rowIdNumber <= 1) {
+        return null;
+    }
+    rowIdNumber--;
+    this.rowId = rowIdNumber.toString() as typeof RowIndexs [keyof typeof RowIndexs];
+    return this.rowId
+}
+moveLeft(){
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    if (columnIdIndex <= 0) {
+        return null;
+    }        
+    this.columnId = getColumnIndexArray()[columnIdIndex - 1];
+    return this.columnId
+}
+moveRight(){
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    if (columnIdIndex >= (getColumnIndexArray().length - 1)) return null;   
+    this.columnId = getColumnIndexArray()[columnIdIndex + 1];
+    return this.columnId
+}    
+moveUpLeft() {
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    if(rowIdNumber >= getRowIndexArray().length || columnIdIndex <= 0) return null;
+    return {
+        columnId: this.moveLeft() as ColumnIds,
+        rowId: this.moveUp() as RowIds
+    }
+}
+moveUpRight() {
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    if(rowIdNumber >= getRowIndexArray().length || columnIdIndex >= (getColumnIndexArray().length - 1)) return null;
+    return {
+        columnId: this.moveRight() as ColumnIds,
+        rowId: this.moveUp() as RowIds
+    }
+
+}moveDownLeft () {
+
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    console.log("This is the current ColumnIdIndex: " + rowIdNumber);
+    console.log("This is the current ColumnId:" + this.rowId);
+    if (rowIdNumber <= 1|| columnIdIndex <= 0) {
+        return null;
+        }    
+    return {
+        columnId: this.moveLeft() as ColumnIds,
+        rowId: this.moveDown() as RowIds
+    }
+}
+moveDownRight () {
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    if (rowIdNumber <= 1|| columnIdIndex >= (getColumnIndexArray().length - 1)) return null;
+    return {
+        columnId: this.moveRight() as ColumnIds,
+        rowId: this.moveDown() as RowIds
+    }
+}
+}
+
+/*
+Left combinations
+lx3 ux1
+lx1 ux3
+lx3 dx1
+lx1 dx3
+
+Right combinations
+rx3 ux1
+rx1 ux3
+rx3 dx1
+rx1 dx3
+*/
+
+export class KnightPositions extends PositionParent implements KnightPositionBlueprint {
+// eslint-disable-next-line @typescript-eslint/no-useless-constructor
+constructor(columnId: ColumnIds, rowId: RowIds) {
+    super(columnId, rowId)
+}
+moveUpLeft()  {
+    const possibleTileIds: BoardPosition[] = []
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    let potentialRowId: RowIds | null = null;
+    if(!((rowIdNumber + 2) > getRowIndexArray().length) && !((columnIdIndex - 1) <= 0)) {
+        const newRowId: number = rowIdNumber + 2;
+        potentialRowId = newRowId.toString() as RowIds;
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex -1],
+            rowId: potentialRowId
+        }    
+        possibleTileIds.push(newBoardPosition);
+    }
+    if(!((columnIdIndex - 2) < 0)) {
+        const newRowId: number = rowIdNumber + 1;
+        potentialRowId = newRowId.toString() as RowIds;
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex - 2],
+            rowId: potentialRowId
+        }
+        possibleTileIds.push(newBoardPosition);  
+    };
+    if(possibleTileIds.length > 0) return possibleTileIds;
+    return null;
+}
+moveUpRight(): null | BoardPosition[]  {
+    const possibleTileIds: BoardPosition[] = []
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    let potentialRowId: RowIds | null = null;
+    if(!((rowIdNumber + 2) > getRowIndexArray().length) && !((columnIdIndex + 1) <= 0)) {
+        const newRowId: number = rowIdNumber + 2;
+        potentialRowId = newRowId.toString() as RowIds;
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex + 1],
+            rowId: potentialRowId
+        }    
+        possibleTileIds.push(newBoardPosition);
+    }
+    if(!((columnIdIndex + 2) < 0)) {
+        const newRowId: number = rowIdNumber + 1;
+        potentialRowId = newRowId.toString() as RowIds;
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex + 2],
+            rowId: potentialRowId
+        }
+        possibleTileIds.push(newBoardPosition);  
+    };
+    if(possibleTileIds.length > 0) return possibleTileIds;
+    return null;
+}
+moveDownLeft(): null | BoardPosition[]  {
+    const possibleTileIds: BoardPosition[] = []
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    let potentialRowId: RowIds | null = null;
+    if(!((rowIdNumber - 2) <= 0) && !((columnIdIndex - 1) < 0)) {
+        const newRowId: number = rowIdNumber - 2
+        potentialRowId = newRowId.toString() as RowIds;
+        console.log("new rowID: " + newRowId)
+        console.log("Potential rowID: " + potentialRowId)
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex - 1],
+            rowId: potentialRowId
+        }    
+        possibleTileIds.push(newBoardPosition);
+    }
+    if(!((columnIdIndex - 2) < 0)) {
+        const newRowId: number = rowIdNumber - 1
+        potentialRowId = newRowId.toString() as RowIds;
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex - 2],
+            rowId: potentialRowId
+        }
+        possibleTileIds.push(newBoardPosition);  
+    };
+    if(possibleTileIds.length > 0) return possibleTileIds;
+    return null;
+}
+moveDownRight(): null | BoardPosition[]  {
+    const possibleTileIds: BoardPosition[] = []
+    let rowIdNumber = parseInt(this.rowId);
+    const columnIdIndex = getColumnIndexArray().indexOf(this.columnId);
+    let potentialRowId: RowIds | null = null;
+    if(!((rowIdNumber - 2) <= 0) && !((columnIdIndex + 1) >= getColumnIndexArray().length)) {
+        const newRowId: number = rowIdNumber - 2
+        potentialRowId = newRowId.toString() as RowIds;
+        console.log("new rowID: " + newRowId)
+        console.log("Potential rowID: " + potentialRowId)
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex + 1],
+            rowId: potentialRowId
+        }    
+        possibleTileIds.push(newBoardPosition);
+    }
+    if(!((columnIdIndex + 2) >= getColumnIndexArray().length)) {
+        const newRowId: number = rowIdNumber - 1
+        potentialRowId = newRowId.toString() as RowIds;
+        const newBoardPosition: BoardPosition = {
+            columnId: getColumnIndexArray()[columnIdIndex + 2],
+            rowId: potentialRowId
+        }
+        possibleTileIds.push(newBoardPosition);  
+    };
+    if(possibleTileIds.length > 0) return possibleTileIds;
+    return null;
+}
+}  
